110851c708565cad97a5a3a953c664eff7107adf
unknown <romiramdhani84@gmail.com>
2021-01-19 13:00:52 +0700

    mengatur templete admin di laravel

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// admin route
+Route::group(['prefix'=> 'admin', 'middleware'=> ['auth']], function ()
+{
+    Route::get('/', function () {
+        return view('adminnice.index');
+    });
+    Route::get('/form-basic', function () {
+        return view('adminnice.form-basic');
+    });
+    Route::get('/icon-material', function () {
+        return view('adminnice.icon-material');
+    });
+    Route::get('/pages-profile', function () {
+        return view('adminnice.pages-profile');
+    });
+    Route::get('/starter-kit', function () {
+        return view('adminnice.starter-kit');
+    });
+    Route::get('/table-basic', function () {
+        return view('adminnice.table-basic');
+    });
+    Route::get('/error', function () {
+        return view('adminnice.error');
+    });
+});
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin', function () {
-    return view('adminnice.index');
-});
-Route::get('/form-basic', function () {
-    return view('adminnice.form-basic');
-});
-Route::get('/icon-material', function () {
-    return view('adminnice.icon-material');
-});
-Route::get('/pages-profile', function () {
-    return view('adminnice.pages-profile');
-});
-Route::get('/starter-kit', function () {
-    return view('adminnice.starter-kit');
-});
-Route::get('/table-basic', function () {
-    return view('adminnice.table-basic');
-});
-Route::get('/error', function () {
-    return view('adminnice.error');
-});
-
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
